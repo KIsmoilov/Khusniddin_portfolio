b2418839cf7b7797fdf5bf2deca7d1f6ce087059
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-06-06 21:11:06 +0500

    Add all the tech stack to the projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  SiJavascript, SiRuby, SiRubyonrails, SiCss3, SiHtml5,
+  SiRedux, SiTailwindcss, SiWebpack, SiEslint, SiJest,
+} from 'react-icons/si';
+import { FaReact } from 'react-icons/fa';
 import Title from '../layouts/Title';
 import {
   carRental, hpChar, math, space, foodApp, leaderboard,
@@ -19,10 +24,16 @@ const Projects = () => (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
       <ProjectsCard
         title="Rental Car App"
-        des="This app allows users to reserve a car, add new cars and delete existing ones."
+        des="This is a user-friendly car rental management app that allows users to reserve cars for rental, add new cars, and delete existing ones."
         src={carRental}
         githubLink="https://github.com/wassimchakib/Car-Rental-Front-End"
         demoLink="https://car-rental-front-end.vercel.app/"
+        icons={[
+          <SiTailwindcss key="tailwindcss" />,
+          <FaReact key="react" />,
+          <SiRedux key="redux" />,
+          <SiRuby key="ruby" />,
+          <SiRubyonrails key="rubyonrails" />]}
       />
       <ProjectsCard
         title="Space Traveler`s Hub"
@@ -31,6 +42,12 @@ const Projects = () => (
         src={space}
         githubLink="https://github.com/KIsmoilov/Space-Traveler-s-Hub"
         demoLink="https://space-travelers-hub-astn.onrender.com/"
+        icons={[
+          <SiHtml5 key="html" />,
+          <SiCss3 key="css" />,
+          <SiJavascript key="javascript" />,
+          <FaReact key="react" />,
+          <SiRedux key="redux" />]}
       />
       <ProjectsCard
         title="Harry Potter"
@@ -39,28 +56,52 @@ const Projects = () => (
         src={hpChar}
         githubLink="https://github.com/KIsmoilov/React-Capstone_Harry-Potter-Characters"
         demoLink="https://hp-characters-qmdj.onrender.com/"
+        icons={[
+          <SiHtml5 key="html" />,
+          <SiCss3 key="css" />,
+          <SiJavascript key="javascript" />,
+          <FaReact key="react" />,
+          <SiRedux key="redux" />]}
       />
       <ProjectsCard
         title="Leaderboard App"
-        des="This is a leaderboard website that displays scores submitted by different players. It also allows you to submit your score. All data is preserved by the external Leaderboard API service."
+        des="This is a leaderboard app that displays scores submitted by different players and allows you to submit your score. The data is stored in the external Leaderboard API."
         src={leaderboard}
         githubLink="https://github.com/KIsmoilov/Leaderboard"
         demoLink="https://kismoilov.github.io/Leaderboard/"
+        icons={[
+          <SiHtml5 key="html" />,
+          <SiCss3 key="css" />,
+          <SiJavascript key="javascript" />,
+          <SiWebpack key="webpack" />,
+          <SiEslint key="eslint" />]}
       />
       <ProjectsCard
         title="Food App"
-        des="The app allows the user to see various kinds of meal categories with the functionalities of like and comment features.
-        The MEALDB API has been used to provide meal category data for our Food App and built the web app around it."
+        des="This is a food app that allows users to see various kinds of meal categories with like and comment features.
+        The MEALDB API data has been used to build the app."
         src={foodApp}
         githubLink="https://github.com/KIsmoilov/JS-Capstone-Project"
         demoLink="https://kismoilov.github.io/JS-Capstone-Project/"
+        icons={[
+          <SiHtml5 key="html" />,
+          <SiCss3 key="css" />,
+          <SiJavascript key="javascript" />,
+          <SiWebpack key="webpack" />,
+          <SiJest key="jest" />]}
       />
       <ProjectsCard
         title="Math Magicians"
-        des="'Math magicians' is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and read a random math-related quote."
+        des="This app is for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and read a random math-related quote."
         src={math}
         githubLink="https://github.com/KIsmoilov/math-magicians"
         demoLink="https://calculator-by-math-magicians.onrender.com/"
+        icons={[
+          <SiHtml5 key="html" />,
+          <SiCss3 key="css" />,
+          <SiJavascript key="javascript" />,
+          <FaReact key="react" />,
+          <SiEslint key="eslint" />]}
       />
     </div>
   </section>
--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,10 +1,11 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BsGithub } from 'react-icons/bs';
 import { FaGlobe } from 'react-icons/fa';
 
 const ProjectsCard = ({
-  title, des, src, githubLink, demoLink,
+  title, des, src, githubLink, demoLink, icons,
 }) => (
   <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
     <div className="w-full h-60 overflow-hidden rounded-lg border-solid border-2 border-white-600">
@@ -42,6 +43,13 @@ const ProjectsCard = ({
         <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
           {des}
         </p>
+        <div className="flex gap-4 mt-5">
+          {icons && icons.length > 0 && icons.map((icon, index) => (
+            <span key={index} className="bannerIcon">
+              {icon}
+            </span>
+          ))}
+        </div>
       </div>
     </div>
   </div>
@@ -53,6 +61,7 @@ ProjectsCard.propTypes = {
   src: PropTypes.string.isRequired,
   githubLink: PropTypes.string.isRequired,
   demoLink: PropTypes.string.isRequired,
+  icons: PropTypes.arrayOf(PropTypes.element).isRequired,
 };
 
 export default ProjectsCard;
